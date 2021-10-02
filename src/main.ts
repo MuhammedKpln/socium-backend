95e90cb8d145dbef8ad61c907648b8f37d3bbc4e
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-10-02 22:25:59 +0200

    redis

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,7 @@ import * as redis from 'redis';
 import { readFile } from 'fs/promises';
 
 export const redisUrl =
-  process.env.NODE_ENV !== 'production'
-    ? null
-    : { url: process.env.REDIS_TLS_URL };
+  process.env.NODE_ENV !== 'production' ? null : { url: process.env.REDIS_URL };
 
 export const redisClient = redis.createClient(redisUrl);
 
