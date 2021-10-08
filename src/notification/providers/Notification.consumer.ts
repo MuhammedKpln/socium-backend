80c6aa99a86df2ed12db2d9fc4b5e08b5c2dc7f9
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-10-09 01:33:18 +0200

    replace

--- src/notification/providers/Notification.consumer.ts
+++ src/notification/providers/Notification.consumer.ts
@@ -26,7 +26,7 @@ export class NotificationConsumer {
   ) {
     const adminConfig: firebase.ServiceAccount = {
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     };
     // Initialize the firebase admin app
