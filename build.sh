06de1cf1928ff597c6c922960a75b712c835e516
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-10-22 16:23:32 +0200

    build

---
+++ build.sh
@@ -0,0 +1,7 @@
+export DATABASE_URL=postgres://derdevam:kovUmeB8*!cH#5@localhost:5432/derdevam 
+export REDIS_URL=redis://redis:6379 
+export NODE_ENV=production 
+export PAGINATION_LIMIT=15 
+export SECRET_KEY=secret
+
+yarn build
