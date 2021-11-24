b55108eccbc4ff177aa80295edf927b4b688e753
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-11-25 00:25:19 +0100

    remove typeorm subscriber

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { UserLikeSubscriber } from './entities/userlike.subscriber';
 import { LikesResolver } from './likes.resolver';
 import { LikesService } from './likes.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [LikesService, UserLikeSubscriber, LikesResolver],
+  providers: [LikesService, LikesResolver],
   exports: [LikesService],
 })
 export class LikesModule {}
