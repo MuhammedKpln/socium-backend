69612c3e9f6a734fb21039f983378cf15d03212d
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-11-25 18:37:06 +0100

    remove unused starservices

--- src/star/star.service.ts
+++ src/star/star.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/auth/entities/user.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Repository } from 'typeorm';
-import { Star } from './entities/star.entity';
 
 @Injectable()
 export class StarService {
